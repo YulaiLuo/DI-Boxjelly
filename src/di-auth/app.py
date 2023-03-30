f2fb6139780c7603c6ebf24220aaad62c01b5c15
KunxiSun <sunkunxi@qq.com>
2023-03-30 10:49:18 +0800

    base gateway

--- src/di-auth/app.py
+++ src/di-auth/app.py
@@ -9,9 +9,10 @@ Example:
     >>> app = Flask(__name__)
     >>> app.run()
 """
-
-from flask import Flask
+import consul
+from flask import Flask, jsonify
 from resources import api, mongo, bcrypt, mail, jwt
+import socket
 
 # Initialize the configuration of flask app
 import app_config
@@ -24,10 +25,44 @@ bcrypt.init_app(app)
 mail.init_app(app)
 jwt.init_app(app)
 
+@app.route('/health')
+def health():
+    response = jsonify({'code': '200', 'msg': 'success'})
+    response.status_code = 200
+    return response
+
+# def register_service_to_consul(service_name, service_port, consul_host='127.0.0.1', consul_port=8500):
+
+#     c = consul.Consul(host=consul_host, port=consul_port)
+
+#     # Get this machine's IP address
+#     ip_address = '127.0.0.1'
+#     # ip_address = socket.getaddrinfo(socket.gethostname(), None)
+
+#     c.agent.service.register(
+#         service_name,
+#         address=ip_address,
+#         port=service_port,
+#         check=consul.Check.http(
+#             f'http://{ip_address}:{service_port}/health',
+#             interval='10s',
+#             timeout='1s'
+#         )
+#     )
+
 if __name__ == '__main__':
 
     HOST = '0.0.0.0'
     PORT= 8001
-    DEBUG = True
+    app.run(debug=True, host=HOST, port=PORT)
 
-    app.run(debug=DEBUG, host=HOST, port=PORT)
+    # import sys
+    # service_name = sys.argv[1]
+    # # service_host = sys.argv[2]
+    # service_port = int(sys.argv[2])
+
+    # print(service_name, service_port)
+
+    # register_service_to_consul(service_name, service_port,consul_host='127.0.0.1',consul_port=8500)
+
+    # app.run(debug=True, host=service_host, port=service_port)
--- src/di-gateway/app.py
+++ src/di-gateway/app.py
@@ -1,101 +1,37 @@
 from flask import Flask, request, jsonify
 import requests
-from flask_jwt_extended import JWTManager, jwt_required,set_access_cookies, set_refresh_cookies
+
 app = Flask(__name__)
 
-DI_AUTH_URL = "http://di_auth:8001"
-DI_ONTOSERVER_URL = "http://di_ontoserver:8001"
-
-
-jwt_settings = {
-    'JWT_TOKEN_LOCATION': ['cookies'],
-    "JWT_SECRET_KEY":"di",
-    "JWT_ALGORITHM":"HS256",
-    'JWT_ACCESS_TOKEN_EXPIRES':3600,
+# Service address
+service_map = {
+    "auth": "http://127.0.0.1:8001/di_auth",
+    "map": "http://127.0.0.1:8002/di_map"
 }
-# Initialize
-app.config.update(jwt_settings)
-jwt = JWTManager(app)
 
-# @app.after_request
-# def refresh_expiring_jwts(response):
-#     try:
-#         exp_timestamp = get_jwt()["exp"]
-#         now = datetime.now(timezone.utc)
-#         target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
-#         if target_timestamp > exp_timestamp:
-#             access_token = create_access_token(identity=get_jwt_identity())
-#             set_access_cookies(response, access_token)
-#         return response
-#     except (RuntimeError, KeyError):
-#         # Case where there is not a valid JWT. Just return the original response
-#         return response
+@app.route("/<path:path>", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+def gateway(path):
+    # Get service name from path
+    service_name = path.split("/")[0]
 
-@app.route('/login', methods=['POST'])
-def login():
-    """
-    Login a user.
-    Returns:
-        Reponse: if status code is 200, return the user info and access token.
-                 if status code is not 200, return the error message.
-    """
-    # send the login request to the login service
-    s_response = requests.post(f'{DI_AUTH_URL}/di_auth/login', json=request.get_json())
-    print(s_response)
-    if s_response.status_code == 200:
+    if service_name not in service_map:
+        response = jsonify(code=404 ,err="Service not found")
+        response.status_code = 404
+        return response
 
-        # get the cookies from the service response
-        access_token = s_response.json().get("data").get("access_token")
-        refresh_token = s_response.json().get("data").get("refresh_token")
+    target_url = service_map[service_name] + "/" + "/".join(path.split("/")[1:])
+    print("target_url ",target_url)
+    response = requests.request(
+        method=request.method,
+        url=target_url,
+        headers={key: value for (key, value) in request.headers if key != "Host"},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
 
-        c_response = jsonify(code=200, msg='success')
-
-        # set tthe cookies into the client response
-        set_access_cookies(c_response, access_token)
-        set_refresh_cookies(c_response, refresh_token)
-        return c_response
-    else:
-        return s_response.json(), s_response.status_code
-
-
-@app.route("/register", methods=["POST"])
-def register():
-    """
-    Register a new user.
-    Try: curl -X POST http://localhost:8000/register -H 'Content-Type: application/json' -d '{"email":"diboxjelly@gmail.com","password":"123"}'
-    Returns:
-        Response: if status code is 200, return the user info and access token.
-                  if status code is not 200, return the error message.
-    """
-    print("in register")
-    # send the register request to the register service
-    print("request:", request.json)
-    response = requests.post(f'{DI_AUTH_URL}/di_auth/register', json=request.get_json())
-    print(response)
-
-    # send the response from the register service to the client
-    return jsonify(response.json()), response.status_code
-
-@app.route("/logout", methods=["POST"])
-@jwt_required
-def logout():
-    """
-    Logout a user.
-    Returns:
-        Response: if status code is 200, user logout success
-                  if status code is not 200, return the error message.
-    """
-    # send the logout request to the register service
-    response = requests.post(f'{DI_AUTH_URL}/di_auth/logout', json=request.get_json())
-
-    # send the response from the logout service to the client
-    return jsonify(response.json()), response.status_code
+    return response.content, response.status_code, response.headers.items()
 
 if __name__ == "__main__":
     HOST = '0.0.0.0'
-    PORT= 8000
-    DEBUG = True
-
-    print("-"*70)
-
-    app.run(debug=DEBUG, host=HOST, port=PORT)
+    PORT = 80
+    app.run(debug=True, host=HOST, port=PORT)
