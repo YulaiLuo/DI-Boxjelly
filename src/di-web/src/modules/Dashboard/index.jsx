91bdf96548fd028f5267e79b66cb42af438cf7c7
ghy99123 <2941879036@qq.com>
2023-04-02 00:28:56 +1100

    feat: implement static header profile in main page

--- src/di-web/src/modules/Dashboard/index.jsx
+++ src/di-web/src/modules/Dashboard/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Outlet, useNavigate, useLocation } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import { UserOutlined, DownOutlined } from '@ant-design/icons';
+import { Layout, Menu, Avatar, Space, Dropdown } from 'antd';
+import { removeTokens } from '../../utils/auth'
 
 const { Sider, Header, Content } = Layout;
 
@@ -8,11 +10,10 @@ export default function Dashboard() {
   const navigate = useNavigate();
   const location = useLocation();
   let selectedPath = location.pathname.split('/').pop();
-  if (selectedPath === '')
-    selectedPath = 'mapping'
+  if (selectedPath === '') selectedPath = 'mapping';
 
   const onMenuItemClick = (item) => {
-    navigate(`/${item.key}`);
+    navigate(`/${item.key}`, {replace: true});
   };
 
   const getSidebarItem = (label, key, children, icon) => ({
@@ -31,6 +32,32 @@ export default function Dashboard() {
     getSidebarItem('Account', 'profile'),
   ];
 
+  const ProfileDropdownItems = [
+    {
+      key: 'profile',
+      label: 'Your Profile',
+    },
+    {
+      key: 'signOut',
+      label: 'Sign out',
+    },
+  ];
+
+  const onSignOutClick = () => {
+    console.log('sign out');
+    removeTokens();
+    navigate('/login', {replace: true})
+  };
+
+  const onProfileClick = () => {
+    console.log('go to profile page');
+  };
+
+  const onDropdownItemClick = (e) => {
+    if (e.key === 'profile') onProfileClick();
+    else if (e.key === 'signOut') onSignOutClick();
+  };
+
   return (
     <>
       <Layout style={{ height: '100vh' }}>
@@ -44,8 +71,19 @@ export default function Dashboard() {
           />
         </Sider>
         <Layout>
-          <Header style={{ background: 'white' }}>
-            <div>Header</div>
+          <Header class="bg-white px-8 py-3 flex">
+            {/* <span class="self-center">Header</span> */}
+
+            <Space class="flex-1 flex justify-end items-center" size={12}>
+              <Avatar icon={<UserOutlined />} size="large" />
+
+              <Dropdown menu={{ items: ProfileDropdownItems, onClick: onDropdownItemClick }}>
+                <div>
+                  <span class="text-lg cursor-pointer mr-2">Daniel</span>
+                  <DownOutlined />
+                </div>
+              </Dropdown>
+            </Space>
           </Header>
           <Content>
             <Outlet />
