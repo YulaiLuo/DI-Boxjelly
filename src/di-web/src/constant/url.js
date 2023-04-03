68822eeb3472d470fc2f1035f6e93b2a27fe7692
KunxiSun <sunkunxi@qq.com>
2023-04-03 23:25:57 +0800

    fix the url in web requests

--- src/di-web/src/constant/url.js
+++ src/di-web/src/constant/url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://101.43.110.249';
+export const BASE_URL = 'http://101.43.110.249:8000';
 
-export const EMAIL_LOGIN_URL = '/di_auth/login/email';
+export const EMAIL_LOGIN_URL = '/auth/login/email';
