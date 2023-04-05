67a1aef1ef0d59051d946d4c2e55c41607ac02a1
KunxiSun <sunkunxi@qq.com>
2023-04-05 23:34:51 +0800

    move unimportant file to test

--- tests/mailgun.py
+++ tests/mailgun.py
@@ -18,8 +18,10 @@ mail = Mail(app)
 
 if __name__ == '__main__':
     with app.app_context():
+
         msg = Message(subject="Hello",
-                      sender=app.config.get("MAIL_USERNAME"),
-                      recipients=["diboxjelly@gmail.com"], # replace with your email for testing
-                      body="This is a test email I sent with Gmail and Python!")
+                sender=app.config.get("MAIL_USERNAME"),
+                recipients=["diboxjelly@gmail.com"], # replace with your email for testing
+                body="This is a test email I sent with Gmail and Python!")
+        
         mail.send(msg)
