2ab679ed9c2964be2a9729368caa2fdd303ee308
Chenyang Dong <zjgdcy@gmail.com>
2023-05-03 17:44:08 +1000

    Change initialisation of flask app for mapping

--- src/di-map/app.py
+++ src/di-map/app.py
@@ -1,15 +1,23 @@
-from flask import Flask, jsonify, request
-import requests
-from api import api
+"""
+This module is responsible for user authentication and authorization.
 
-app = Flask(__name__)
-api.init_app(app)
+The main function including: login, register, logout, and invite member.
 
+Example:
+    >>> from flask import Flask
+    >>> from app_config import app_config
+    >>> app = Flask(__name__)
+    >>> app.run()
+"""
+
+from app import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
 
     HOST = '0.0.0.0'
-    PORT= 8003
+    PORT = 8003
     DEBUG = True
 
     app.run(debug=DEBUG, host=HOST, port=PORT)
