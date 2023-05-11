f6f2f4597c9c31aafe06d1784338e3076150019c
Chenyang Dong <zjgdcy@gmail.com>
2023-05-12 06:21:57 +1000

    feat: Modal for creating mapping task

--- src/di-web/src/modules/MappingHistory/index.jsx
+++ src/di-web/src/modules/MappingHistory/index.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { List, Pagination, Button } from 'antd';
+import { List, Pagination, Button, Modal } from 'antd';
 import { useRequest } from 'ahooks';
 import { getAllMappingTasks, getMappingTaskDetail } from './api';
 import TaskCard from './components/TaskCard';
 import { Spin } from '../../components';
 import { convertKeysToCamelCase } from '../../utils/underlineToCamel';
 import { exportFile } from '../Mapping/api';
+import { FileUploader } from '../../components';
+import { useMessageStore } from '../../store';
+import { createMappingTask } from '../Mapping/api';
 
 export default function MappingHistory() {
   const team_id = '60c879e72cb0e6f96d6b0f65';
@@ -14,6 +17,7 @@ export default function MappingHistory() {
   const PAGE_SIZE = 10;
   const [currentPage, setCurrentPage] = useState(1);
   const navigate = useNavigate();
+  const msgApi = useMessageStore((state) => state.msgApi);
 
   const onGetTaskDetailSuccess = (data) => {
     console.log('data', data);
@@ -37,6 +41,41 @@ export default function MappingHistory() {
   const mappedTasks = tasks.map((task) => {
     return convertKeysToCamelCase(task);
   });
+  const [files, setFiles] = useState([]);
+
+  const [open, setOpen] = useState(false);
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const { loading: createTaskLoading, run: handleCreateMappingTask } = useRequest(
+    createMappingTask,
+    {
+      manual: true,
+      onSuccess: () => {
+        setOpen(false);
+        msgApi.success('Mapping task created successfully');
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
+      },
+    }
+  );
+
+  const onCreateTaskClick = async () => {
+    const uploadedFile = files[0]?.file;
+    // TODO: currently cannot access the real teamId
+    const teamId = '60c879e72cb0e6f96d6b0f65';
+    const boardId = '60c879e72cb0e6f96d6b0f65';
+    handleCreateMappingTask(uploadedFile, teamId, boardId);
+  };
+
+  const onFileUpdate = (files) => {
+    setFiles(files);
+  };
 
   return (
     <div class="p-4 h-full">
@@ -45,14 +84,18 @@ export default function MappingHistory() {
       ) : (
         <div>
           <div class="flex flex-row-reverse mb-4 mt-2">
-            <Button
-              type="primary"
-              onClick={() => {
-                navigate('/mapping');
-              }}
-            >
+            <Button type="primary" onClick={showModal}>
               Create Task
             </Button>
+            <Modal
+              title="Create Mapping Task"
+              open={open}
+              onOk={onCreateTaskClick}
+              confirmLoading={createTaskLoading}
+              onCancel={handleCancel}
+            >
+              <FileUploader files={files} onFileUpdate={onFileUpdate} />
+            </Modal>
           </div>
           <List
             grid={{
