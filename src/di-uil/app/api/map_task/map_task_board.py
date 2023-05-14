fda3024bd9481ddecac561333107d4c19da9d449
KunxiSun <sunkunxi@qq.com>
2023-05-14 21:21:02 +0800

    creat task board

--- src/di-uil/app/api/map_task/map_task_board.py
+++ src/di-uil/app/api/map_task/map_task_board.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from flask import jsonify, request, make_response
-from app.models import MapTask, MapItem
+from app.models import MapTask, MapItem, TaskBoard
 from bson import ObjectId
 from marshmallow import Schema, fields, ValidationError, validates
 from flask import current_app as app
@@ -10,9 +10,39 @@ from io import StringIO
 class GetTaskBoardsInputSchema(Schema):
     team_id = fields.String(required=True)
 
+class PostMapTaskInputSchema(Schema):
+    team_id = fields.String(required=True)
+    name = fields.String(required=True)
+    description = fields.String(required=False)
 
 class MapTaskBoardsResource(Resource):
-   
+
+    def post(self):
+        """Create a new task board
+        """
+        try:
+            in_schema = PostMapTaskInputSchema()
+            in_schema = in_schema.load(request.json)
+        except:
+            return make_response(jsonify(code=400, err="INVALID_INPUT"), 400)
+        
+        try:
+            board = TaskBoard(
+                team_id=ObjectId(in_schema['team_id']),
+                name=in_schema['name'],
+                description=in_schema.get('description','')
+            ).save()
+            data = {
+                "id": str(board.id),
+                "name": board.name,
+                "description": board.description,
+                "create_at": board.create_at,
+                "update_at": board.update_at
+            }
+            return make_response(jsonify(code=200, msg="ok", data=data), 200)
+        except:
+            return make_response(jsonify(code=500, err="INTERNAL_SERVER_ERROR"), 500)
+
     def get(self):
         """Get task board list
         """
