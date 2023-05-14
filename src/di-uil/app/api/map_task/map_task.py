097e4335217edcec10fc2e1c65bd73b49fc10db9
KunxiSun <sunkunxi@qq.com>
2023-05-14 22:58:55 +0800

    move delete method

--- src/di-uil/app/api/map_task/map_task.py
+++ src/di-uil/app/api/map_task/map_task.py
@@ -11,6 +11,11 @@ import codecs
 import csv
 from io import StringIO
 
+class DeleteMapTaskInputSchema(Schema):
+    task_id = fields.String(required=True)
+    team_id = fields.String(required=True)
+    board_id = fields.String(required=True)
+
 class PostMapTaskInputSchema(Schema):
    team_id = fields.String(required=True)
    board_id = fields.String(required=True)
@@ -209,4 +214,42 @@ class MapTaskResource(Resource):
          print(err)
          return make_response(jsonify(code=500, err="INTERNAL_SERVER_ERROR"),500)
 
+   def delete(self):
+      """
+      Soft delete a map task in the database by changing the deleted as True
 
+      Args:
+         task_id (_type_): _description_
+
+      Returns:
+         _type_: _description_
+      """
+      try:
+         in_schema = DeleteMapTaskInputSchema()
+         in_schema = in_schema.load(request.args)
+      except ValidationError as err:
+         return make_response(jsonify(code=400, err="INVALID_INPUT"),404)
+
+      try:
+         # TODO: Check if the task is permitted to be deleted by this user
+
+         # TODO: Check if the task is deleted
+
+         # Change the deleted field as deleted
+         MapTask.objects(id=ObjectId(in_schema['task_id'])).update_one(deleted=True)
+
+         response = jsonify(code=200, msg="ok")
+         response.status_code = 200
+         return response
+
+      except ValidationError as err:
+         print(err)
+         response = jsonify(code=400, err="INVALID_INPUT")
+         response.status_code = 400
+         return response
+
+      except Exception as err:
+         response = jsonify(code=500, err="INTERNAL_SERVER_ERROR")
+         response.status_code = 500
+         return response
+   
