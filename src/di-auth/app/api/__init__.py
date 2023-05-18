48ae571712f32ff0b1853f99070782b7eed5747a
Chenyang Dong <zjgdcy@gmail.com>
2023-05-19 01:21:55 +1000

    fix: invite to create account endpoints changed

--- src/di-auth/app/api/__init__.py
+++ src/di-auth/app/api/__init__.py
@@ -2,10 +2,11 @@ from flask_restful import Api
 from .login import EmailLogin
 from .logout import Logout
 from ..utils import get_mongo, get_bcrypt, get_jwt
-from .team import TeamResource, PendingTeamResource
+from .team import TeamResource
 from .user import UserResource
 from .avatar import AvatarResource
 from .invite import InviteResource, AcceptInviteResource
+
 
 def init_api(app):
     """
@@ -14,7 +15,7 @@ def init_api(app):
     Args:
         app (Flask): The Flask app
     """
-    
+
     # Create the API instance
     api = Api()
 
@@ -25,7 +26,6 @@ def init_api(app):
 
     # Team resource
     api.add_resource(TeamResource, '/auth/team')
-    api.add_resource(PendingTeamResource, '/auth/team/pending')
 
     # User resource
     api.add_resource(UserResource, '/auth/user')
@@ -33,10 +33,11 @@ def init_api(app):
 
     # Invite resource
     api.add_resource(InviteResource, '/auth/team/invite')
-    api.add_resource(AcceptInviteResource, '/auth/team/accept',resource_class_args=(bcrypt,))
-    
-    # api.add_resource(Invite, '/auth/invite',resource_class_args=(mongo, mail))
-    api.add_resource(EmailLogin, '/auth/login/email',resource_class_args=(mongo, bcrypt,))
+    api.add_resource(AcceptInviteResource, '/auth/team/accept',
+                     resource_class_args=(bcrypt,))
+
+    api.add_resource(EmailLogin, '/auth/login/email',
+                     resource_class_args=(mongo, bcrypt,))
     api.add_resource(Logout, '/auth/logout', resource_class_args=(mongo, jwt,))
 
     # Initialize the API
