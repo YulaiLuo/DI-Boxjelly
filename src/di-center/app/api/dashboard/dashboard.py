66e9c91b1db7a668203536b8b244f4f4f01f3247
KunxiSun <sunkunxi@qq.com>
2023-05-19 02:41:33 +0800

    add top tree total mapping

--- src/di-center/app/api/dashboard/dashboard.py
+++ src/di-center/app/api/dashboard/dashboard.py
@@ -1,37 +1,86 @@
 from flask_restful import Resource
 from flask import jsonify, request, make_response
-from app.models import ConceptGroup, Concept, CodeSystem
+from app.models import ConceptGroup, Concept, CodeSystem, MapItem, MapTask
 from flask import current_app as app
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta, MO
+from mongoengine import Q
 import requests
+
+def get_start_of_weeks():
+    """
+    Get the start of this week and last week
+
+    Returns:
+        datetime: the start of this week
+        datetime: the start of last week
+    """
+    start_of_this_week = datetime.now() + relativedelta(weekday=MO(-1))
+    start_of_last_week = start_of_this_week - relativedelta(weeks=1)
+    return start_of_this_week, start_of_last_week
 
 class TopLeftResource(Resource):
 
     def get(self):
+
+        # Calculate the start of this week and the start of the last week
+        start_of_this_week, start_of_last_week = get_start_of_weeks()
+        
+        task_count = MapTask.objects(deleted=False).count()
+        
+        this_week_count = MapTask.objects(deleted=False, create_at__gte=start_of_this_week).count()
+        last_week_count = MapTask.objects(deleted=False, create_at__gte=start_of_last_week, create_at__lt=start_of_this_week).count()
+
+        delta = this_week_count - last_week_count
+
         return make_response(jsonify(code=200, msg="ok", data={
-            "title": "This is a title",
-            "total_number": "100",
-            "delta": "+16% since last week",
-            "percent": 0.16
+            "title": f"Total Tasks: {task_count}",
+            "total_number": f"This week: {this_week_count}",
+            "delta": f"{'+' if delta>0 else '1'}{delta} since last week",
+            "percent": delta/last_week_count*100 if last_week_count > 0 else 0
         }))
 
 class TopMiddleResource(Resource):
 
     def get(self):
+
+        # Calculate the start of this week and the start of the last week
+        start_of_this_week, start_of_last_week = get_start_of_weeks()
+
+        # Get all undeleted MapTask ids
+        undeleted_task_ids = [task.id for task in MapTask.objects(deleted=False)]
+
+        this_week_count = MapItem.objects(Q(task__in=undeleted_task_ids) & Q(create_at__gte=start_of_this_week)).count()
+        last_week_count = MapItem.objects(Q(task__in=undeleted_task_ids) & Q(create_at__gte=start_of_last_week) & Q(create_at__lt=start_of_this_week)).count()
+
+        delta = this_week_count - last_week_count
+        total_count = MapItem.objects(task__in=undeleted_task_ids).count()
+
         return make_response(jsonify(code=200, msg="ok", data={
-            "title": "This is a title",
-            "total_number": "100",
-            "delta": "+16% since last week",
-            "percent": 0.16
+            "title": f"Total texts: {total_count}",
+            "total_number": f"This week: {this_week_count}",
+            "delta": f"{'+' if delta>0 else '1'}{delta} since last week",
+            "percent": delta/last_week_count*100 if last_week_count > 0 else 0
         }))
 
 class TopRightResource(Resource):
 
     def get(self):
+
+        # Calculate the start of this week and the start of the last week
+        start_of_this_week, start_of_last_week = get_start_of_weeks()
+
+        this_week_count = MapItem.objects(status='reviewed', create_at__gte=start_of_this_week).count()
+        last_week_count = MapItem.objects(status='reviewed', create_at__gte=start_of_last_week, create_at__lt=start_of_this_week).count()
+        
+        delta = this_week_count - last_week_count
+        total_count = MapItem.objects(status='reviewed').count()
+
         return make_response(jsonify(code=200, msg="ok", data={
-            "title": "This is a title",
-            "total_number": "100",
-            "delta": "+16% since last week",
-            "percent": 0.16
+            "title": f"Total curated: {total_count}",
+            "total_number": f"This week: {this_week_count}",
+            "delta": f"{'+' if delta>0 else '1'}{delta} since last week",
+            "percent": delta/last_week_count*100 if last_week_count > 0 else 0
         }))
 
 class HelloResource(Resource):
