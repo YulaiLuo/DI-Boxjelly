4094e5049bab89d8bc4cfb0af54e7c84b881ec8d
KunxiSun <sunkunxi@qq.com>
2023-05-18 16:58:29 +0800

    single mapping

--- src/di-center/app/api/dashboard/dashboard.py
+++ src/di-center/app/api/dashboard/dashboard.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource
 from flask import jsonify, request, make_response
 from app.models import ConceptGroup, Concept, CodeSystem
-
+from flask import current_app as app
+import requests
 
 class TopLeftResource(Resource):
 
@@ -53,3 +54,21 @@ class MapItemStatusRatioResource(Resource):
             data.append({'year':str(year),'value':random.randint(1,10),'type':'failed'})
 
         return make_response(jsonify(code=200, msg="ok", data=data))
+
+    
+class PredictSingleResource(Resource):
+
+    def post(self):
+
+        data = request.get_json()
+        if 'text' not in data:
+            return make_response(jsonify(code=400, err="INVALID_INPUT"), 400)
+        
+        text = data['text']
+        map_url = app.config['MAP_SERVICE_URL']
+        res = requests.post(f'{map_url}/map/predict', json={'texts': [text]})
+
+        if res.status_code != 200:
+            return make_response(jsonify(code=500, err="INTERNAL_SERVER_ERROR"), 500)
+
+        return make_response(jsonify(code=200, msg="ok", data=res.json()['data']), 200)
