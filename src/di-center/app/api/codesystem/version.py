02626762020b2f295b2e79b1e6a98fe2ae5dab9b
KunxiSun <sunkunxi@qq.com>
2023-05-20 15:48:07 +0800

    remove create by

--- src/di-center/app/api/codesystem/version.py
+++ src/di-center/app/api/codesystem/version.py
@@ -13,21 +13,15 @@ class CodeSystemVersionResource(Resource):
 
         user_id = request.headers.get('User-ID')
 
-        code_sysems = CodeSystem.objects(deleted=False).order_by('-create_at').all()
-
-        users = {}
-        auth_url = app.config['AUTH_SERVICE_URL']
-        for creater_id in set([creater_id for creater_id in code_sysems.create_by]):
-            users[creater_id] = requests.post(f'{auth_url}/auth/user?{user_id}={creater_id}').json()['data']
+        code_systems = CodeSystem.objects(deleted=False).order_by('-create_at').all()
 
         data = {
             'code_systems':[{
                 'name':code_system.name,
                 'description': code_system.description,
-                'create_by': users[code_system.create_by],
                 'version': code_system.version,
                 'create_at': code_system.create_at,
-            }for code_system in code_sysems]
+            }for code_system in code_systems]
         }
         return make_response(jsonify(code=200, msg='ok', data=data))
 
