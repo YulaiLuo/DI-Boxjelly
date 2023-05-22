127cfdafdb794618836ee631faa5d68e8ecf2179
ghy99123 <2941879036@qq.com>
2023-05-23 00:40:44 +1000

    feat: remove user in team

--- src/di-web/src/modules/TeamProfile/api.js
+++ src/di-web/src/modules/TeamProfile/api.js
@@ -6,5 +6,9 @@ export const getTeamInfo = (team_id) => {
 };
 
 export const getInvitationLink = (team_id) => {
-  return http.get(`${TEAM_INFO_URL}/invite`, {team_id})
-}
+  return http.get(`${TEAM_INFO_URL}/invite`, { team_id });
+};
+
+export const deleteTeamMember = (team_id, user_id) => {
+  return http.deleteData(`${TEAM_INFO_URL}/member`, { team_id, user_id });
+};
