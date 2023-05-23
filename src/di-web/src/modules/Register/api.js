2d2dd645e24ac3f25a52485db59c46fa3af077c6
YulaiLuo <88716060+YulaiLuo@users.noreply.github.com>
2023-05-24 04:26:28 +1000

    fix: no need to have a token when registering Ref #27

--- src/di-web/src/modules/Register/api.js
+++ src/di-web/src/modules/Register/api.js
@@ -3,6 +3,5 @@ import { REGISTER_URL } from '../../utils/constant/url';
 import { getCSRFTokenHeader } from '../../utils/auth';
 
 export const registerUser = (data) => {
-    const headers = getCSRFTokenHeader();
-    return http.postFormData(`${REGISTER_URL}`, data, headers);
+    return http.postFormData(`${REGISTER_URL}`, data);
 };
