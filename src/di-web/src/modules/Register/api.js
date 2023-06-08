c2bdcb6ee9fd758ba1664b205b30e4b6ef1cef91
YulaiLuo <88716060+YulaiLuo@users.noreply.github.com>
2023-06-09 02:41:49 +1000

    Register module comments

--- src/di-web/src/modules/Register/api.js
+++ src/di-web/src/modules/Register/api.js
@@ -1,6 +1,8 @@
+// Import necessary modules and variables
 import http from '../../utils/http';
 import { REGISTER_URL } from '../../utils/constant/url';
 
+// Function to send a POST request to the register endpoint with the provided data
 export const registerUser = (data) => {
     return http.postFormData(`${REGISTER_URL}`, data);
 };
