b75a185a2caac494849258e0004e759ad3b78b91
Chenyang Dong <zjgdcy@gmail.com>
2023-06-08 15:19:06 +1000

    Update avtar path

--- src/di-auth/app_config.py
+++ src/di-auth/app_config.py
@@ -17,14 +17,16 @@ from datetime import timedelta
 class Config:
 
     # MongoDB
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://boxjelly:di_boxjelly90082@localhost:27017/di?authSource=admin'
+    MONGO_URI = os.environ.get(
+        'MONGO_URI') or 'mongodb://boxjelly:di_boxjelly90082@mongo:27017/di?authSource=admin'
 
     # JWT Setting
     JWT_TOKEN_LOCATION = ['cookies']
     JWT_COOKIE_SECURE = False   # Only set flask in development environment
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'di'
-    JWT_ACCESS_TOKEN_EXPIRES = os.environ.get('JWT_ACCESS_TOKEN_EXPIRES') or timedelta(minutes=60)    
+    JWT_ACCESS_TOKEN_EXPIRES = os.environ.get(
+        'JWT_ACCESS_TOKEN_EXPIRES') or timedelta(minutes=60)
     JWT_CSRF_CHECK_FORM = True
 
     # Avatar settings
-    AVATAR_FOLDER = os.path.expanduser('~/data/di-data/di-auth/avatars')
+    AVATAR_FOLDER = '/data/di-data/di-auth/avatars'
