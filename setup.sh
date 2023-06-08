4251b957a7eff523eb19bb2175c74deaa5bcc810
Chenyang Dong <zjgdcy@gmail.com>
2023-06-08 14:25:18 +1000

    Update setup script

--- setup.sh
+++ setup.sh
@@ -36,24 +36,24 @@ then
 fi
 
 # Create the necessary directories
-mkdir -p ~/data/nginx/conf
-mkdir -p ~/data/nginx/html
-mkdir -p ~/data/nginx/ssl
-mkdir -p ~/data/nginx/logs
+mkdir -p ~/data/di-data/nginx/conf
+mkdir -p ~/data/di-data/nginx/html
+mkdir -p ~/data/di-data/nginx/ssl
+mkdir -p ~/data/di-data/nginx/logs
 mkdir -p ~/data/di-data/di-auth/avatars
-mkdir -p ~/data/di-data/di-map/medtcat_model
+mkdir -p ~/data/di-data/di-map/medcat_model
 
 # Copy nginx configuration file in the di-web directory to the desired location
-cp -r nginx.conf ~/data/nginx/conf/ || { echo "Failed to copy files from the di-web directory."; exit 1; }
+cp -r nginx.conf ~/data/di-data/nginx/conf/ || { echo "Failed to copy files from the di-web directory."; exit 1; }
 
 # Copy all files in the build directory to the desired location
-cp -r build/* ~/data/nginx/html/ || { echo "Failed to copy files from the build directory."; exit 1; }
+cp -r build/* ~/data/di-data/nginx/html/ || { echo "Failed to copy files from the build directory."; exit 1; }
 
 # Navigate back to the root directory
 cd ../..
 
 # Copy the medcat model folder to the desired location
-cp -r src/di-map/medtcat_model/* ~/data/di-data/di-map/medtcat_model/ || { echo "Failed to copy files from the medcat_model directory. Please ensure you unzip the downloaded model, rename the folder as `medtcat_model` and move it under `di-map` folder."; exit 1; }
+cp -r src/di-map/medcat_model/* ~/data/di-data/di-map/medcat_model/ || { echo "Failed to copy files from the medcat_model directory. Please ensure you unzip the downloaded model, rename the folder as medcat_model and move it under di-map folder."; exit 1; }
 
 # Copy the default avatar
 cp src/di-auth/default.jpg ~/data/di-data/di-auth/avatars/ || { echo "Failed to copy the default avatar."; exit 1; }
